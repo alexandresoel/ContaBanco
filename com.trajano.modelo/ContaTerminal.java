a9687e4cae24411af6a74e54a6894bb6f8bc48e0
alexandresoel <alexandre_soel@hotmail.com>
2024-08-01 10:03:15 -0300

    feat: adicionando o desafio da DIO

---
+++ com.trajano.modelo/ContaTerminal.java
@@ -0,0 +1,24 @@
+package com.trajano.modelo;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Por favor digite o seu nome: ");
+        String nomeCliente = entrada.nextLine();
+        System.out.println("Por favor digite o sua Agencia: ");
+        String agencia = entrada.nextLine();
+        System.out.println("Por favor digite o numero da sua conta: ");
+        int numero = entrada.nextInt();
+        System.out.println("Por favor digite o numero da sua conta: ");
+        double saldo = entrada.nextDouble();
+
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é " +agencia+
+                ", conta "+numero+" e seu saldo "+ saldo+" já está disponível para saque");
+
+
+    }
+}
